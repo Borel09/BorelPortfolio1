2fa62766b3b5e4b5f107c2a69df5160634d1f08b
Ryan Borel <rjborel@gmail.com>
2022-01-18 21:49:03 -0600

    version 1.13

--- bg-flipper/complex-app.js
+++ bg-flipper/complex-app.js
@@ -7,17 +7,17 @@ colorBtn.addEventListener('click', colorChange);
 
 
 //FUNCTIONS
-function colorChange(arr){
+function colorChange(arr) {
     const background = document.getElementById('everything');
     const chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     let charLimit = 6;
     let result = "#";
 
-    for(let i = 0; i < charLimit; i++){
+    for (let i = 0; i < charLimit; i++) {
         let randIndex = Math.floor(Math.random() * chars.length)
         result += chars[randIndex]
     }
     background.style.backgroundColor = result;
     spanValue.innerHTML = result;
+    spanValue.style.color = result;
 }
-
